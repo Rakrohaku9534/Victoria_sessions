67b75cc15e0833995fc03d36c58a0bae0b2f3a14
Rakrohaku9534 <Rakrohaku9534@github.com>
2023-09-02 12:09:22 +0700

    main

---
+++ config.js
@@ -0,0 +1,182 @@
+
+
+// Selamat datang di source code Victoria Rosalind
+// Terima kasih sudah mengunakan SC ini
+// Terima kasih juga sudh mampir ke sc ku semoga kalian dpt inovasi baru (Untuk para sepuh).
+// Jangan lupa subscribe Channel Clayza Aubert karna sc ini bakal saya update terus, dengan fitur yang gk pasaran.
+// Mohon bantuanya semuanya... karna saya masih pemula.
+
+
+
+
+import { watchFile, unwatchFile } from 'fs'
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import moment from 'moment-timezone'
+import { group } from 'console'
+import PhoneNumber from 'awesome-phonenumber'
+
+/*============= WAKTU =============*/
+let wibh = moment.tz('Asia/Jakarta').format('HH')
+let wibm = moment.tz('Asia/Jakarta').format('mm')
+let wibs = moment.tz('Asia/Jakarta').format('ss')
+let wktuwib = `${wibh} H ${wibm} M ${wibs} S`
+
+let d = new Date(new Date + 3600000)
+let locale = 'id'
+let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
+let week = d.toLocaleDateString(locale, { weekday: 'long' })
+let date = d.toLocaleDateString(locale, {
+  day: 'numeric',
+  month: 'long',
+  year: 'numeric'
+})
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+
+/// Setting Dari Sini
+global.owner = [
+  ['6285786539008', 'Fahrul', true],
+]
+global.mods = []
+global.prems = []
+global.nomorbot = '6285712408309'
+global.nomorown = '6285786539008'
+global.nomorown1 = '6285786539008'
+global.nomorown2 = '6285786539008'
+global.nomorwa = '6285786539008'
+global.readMore = readMore
+global.author = 'Fahrul'
+global.namebot = '​Victoria Rosalind'
+global.wm = '©Victoria Rosalind By Fahrul'
+global.watermark = wm
+global.wm2 = '©Victoria Rosalind By Fahrul'
+global.botdate = `Date: ${week} ${date}\nTime: ${wktuwib}`
+global.bottime = `Time: ${wktuwib}`
+global.titlebot = `​Victoria Rosalind`
+global.stickpack = '​©Victoria Rosalind'
+global.stickauth = `By Fahrul`
+global.week = `${week} ${date}`
+global.wibb = `${wktuwib}`
+global.nameown1 = 'Fahrul' // Optional
+global.nameown2 = 'Fahrul' // Optional
+global.thumb = 'https://telegra.ph/file/5ece44caa4d1730178656.jpg' // gambar di menu
+global.leave = 'https://i.postimg.cc/yxvn10Ls/ssstik-io-1689170610318.jpg' /// Untuk BG leave
+
+//Link Social Media Ganti Aja Kalau Ga Punya Biarin Aja
+global.myweb = 'https://fahrul.my.id' //website
+global.sig = 'https://www.instagram.com/fahrul_mt' //instagram
+global.sgh = 'https://github.com/Fahrul6868' //github
+global.sgc = 'https://chat.whatsapp.com/FSbrP6LWWm4A8i8uGdyDTs' //group whatsapp
+global.sdc = '-' //discord
+global.snh = 'https://www.youtube.com/c/fahrul2951' //youtube
+
+//Donasi/Payment
+global.pdana = '085786539008' // Dana
+global.povo = '085786539008' // Ovo
+global.pgopay = '085786539008' // Gopay
+
+/// Api keys
+global.xyro = 'oN5NrJ9pYx' //xyroine api key
+global.clayza = 'WnozZzMprW' //clayzaaubert api key
+global.lolkey = '9ec9ae4576890debb36e2db0' // lol human api key
+global.rose = '' // rose api key
+global.uptime = 'u2069112-9044f062bc338a615a82ed6b' // Masukin APIKEY uptimerobot kamu disini ===== https://uptimerobot.com/dashboard?ref=website-header#mySettings
+global.openai = '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'  //api key bisa didapatkan dari https://openai.com/api/
+global.org = 'org-lSi0BjwZIYjlByj3OVi8NGHk'  //openAI Organization name
+global.deepai = ''
+
+/// TEXT
+global.stiker_wait = '_Sedang Di Proses, Mohon Tunggu_.....'
+global.wait = '_Sedang Di Proses, Mohon Tunggu_....'
+global.eror = 'Eror, Mohon Coba Lagi Nanti'
+global.lopr = 'Ⓟ' //LOGO PREMIUM ON MENU.JS
+global.lolm = 'Ⓛ' //LOGO LIMIT/FREE ON MENU.JS
+global.multiplier = 69
+
+/// flaaa
+global.flaaa = [
+  'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=',
+  'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&text=',
+  'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=',
+  'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=',
+  'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text=']
+
+// The higher, The harder levelup
+global.rpg = {
+  emoticon(string) {
+    string = string.toLowerCase()
+    let emot = {
+      agility: '🤸‍♂️',
+      arc: '🏹',
+      armor: '🥼',
+      bank: '🏦',
+      bibitanggur: '🍇',
+      bibitapel: '🍎',
+      bibitjeruk: '🍊',
+      bibitmangga: '🥭',
+      bibitpisang: '🍌',
+      bow: '🏹',
+      bull: '🐃',
+      cat: '🐈',
+      chicken: '🐓',
+      common: '📦',
+      cow: '🐄',
+      crystal: '🔮',
+      darkcrystal: '♠️',
+      diamond: '💎',
+      dog: '🐕',
+      dragon: '🐉',
+      elephant: '🐘',
+      emerald: '💚',
+      exp: '✉️',
+      fishingrod: '🎣',
+      fox: '🦊',
+      gems: '🍀',
+      giraffe: '🦒',
+      gold: '👑',
+      health: '❤️',
+      horse: '🐎',
+      intelligence: '🧠',
+      iron: '⛓️',
+      keygold: '🔑',
+      keyiron: '🗝️',
+      knife: '🔪',
+      legendary: '🗃️',
+      level: '🧬',
+      limit: '🌌',
+      lion: '🦁',
+      magicwand: '⚕️',
+      mana: '🪄',
+      money: '💵',
+      mythic: '🗳️',
+      pet: '🎁',
+      petFood: '🍖',
+      pickaxe: '⛏️',
+      pointxp: '📧',
+      potion: '🥤',
+      rock: '🪨',
+      snake: '🐍',
+      stamina: '⚡',
+      strength: '🦹‍♀️',
+      string: '🕸️',
+      superior: '💼',
+      sword: '⚔️',
+      tiger: '🐅',
+      trash: '🗑',
+      uncommon: '🎁',
+      upgrader: '🧰',
+      wood: '🪵'
+    }
+    let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
+    if (!results.length) return ''
+    else return emot[results[0][0]]
+  }
+}
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
